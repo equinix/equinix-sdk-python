bba5e2c1db9c34ce3eb242a1b00bd01b4ca0ac1d
Charles Treatman <ctreatman@equinix.com>
2024-07-23 09:02:47 -0500

    fix: read version from file at build time

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages  # noqa: H301
 # prerequisite: setuptools
 # http://pypi.python.org/pypi/setuptools
 NAME = "equinix"
-VERSION = "0.1.0"
+VERSION = open('version').read()
 PYTHON_REQUIRES = ">=3.7"
 REQUIRES = [
     "urllib3 >= 1.25.3, < 2.1.0",
