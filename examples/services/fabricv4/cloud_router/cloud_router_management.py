153a1339406a6a70fe263d6c8fa07f6191b55caa
jkallem <jkallem@equinix.com>
2024-08-21 12:33:06 -0700

    Resolving the Comments for Example

--- examples/services/fabricv4/cloud_router/cloud_router_management.py
+++ examples/services/fabricv4/cloud_router/cloud_router_management.py
@@ -9,20 +9,6 @@ from equinix.services import fabricv4
 
 """
     Create a Fabric Cloud Router (FCR) using the provided request payload.
-
-    Args:
-        fcr_request: An instance of the request payload object for creating a Fabric Cloud Router.
-
-    Returns:
-        str: The UUID of the created Fabric Cloud Router.
-
-    Steps:
-        1. Print the FCR request payload.
-        2. Initialize the Equinix Fabric client.
-        3. Call the create_cloud_router API method with the request payload.
-        4. Parse and format the API response into JSON.
-        5. Print the creation response and the UUID of the created FCR.
-        6. Return the UUID of the created FCR.
 """
 
 
@@ -43,18 +29,6 @@ def create_fcr(fcr_request):
 
 """
     Retrieve the details of an existing Fabric Cloud Router (FCR) by its UUID.
-
-    Args:
-        fcr_uuid (str): The UUID of the Fabric Cloud Router to retrieve.
-
-    Returns:
-        None: This method prints the details of the FCR to the console.
-
-    Steps:
-        1. Initialize the Equinix Fabric client.
-        2. Call the get_cloud_router_by_uuid API method with the UUID.
-        3. Parse and format the API response into JSON.
-        4. Print the response with the FCR details.
 """
 
 
@@ -71,19 +45,6 @@ def get_fcr_uuid(fcr_uuid):
 
 """
     Update an existing Fabric Cloud Router (FCR) using the provided UUID and update payload.
-
-    Args:
-        fcr_uuid (str): The UUID of the Fabric Cloud Router to update.
-        update_payload: An instance of the update payload object containing the updates for the FCR.
-
-    Returns:
-        None: This method prints the update response to the console.
-
-    Steps:
-        1. Initialize the Equinix Fabric client.
-        2. Call the update_cloud_router_by_uuid API method with the UUID and update payload.
-        3. Parse and format the API response into JSON.
-        4. Print the response with the updated FCR details.
 """
 
 
@@ -100,17 +61,6 @@ def update_fcr_by_uuid(fcr_uuid, update_payload):
 
 """
     Delete an existing Fabric Cloud Router (FCR) using the provided UUID.
-
-    Args:
-        fcr_uuid (str): The UUID of the Fabric Cloud Router to delete.
-
-    Returns:
-        None: This method prints the deletion response to the console.
-
-    Steps:
-        1. Initialize the Equinix Fabric client.
-        2. Call the delete_cloud_router_by_uuid API method with the UUID.
-        3. Print the deletion response.
 """
 
 
--- examples/services/fabricv4/oauth2/configure_client_credentials.py
+++ examples/services/fabricv4/oauth2/configure_client_credentials.py
@@ -15,39 +15,6 @@ def get_equinix_fabric_client():
        environment variables `EQUINIX_API_CLIENTID` and `EQUINIX_API_CLIENTSECRET`. These
        credentials are then used to obtain an OAuth2 access token, which is subsequently
        used to configure the Equinix Fabric API client.
-
-       The method performs the following steps:
-       1. Retrieves the `client_id` and `client_secret` from environment variables.
-       2. Creates an `OAuth2Service` instance using the client credentials.
-       3. Requests an access token using the `client_credentials` grant type.
-       4. Configures the Equinix Fabric API client with the obtained access token.
-       5. Returns the initialized `fabricv4.ApiClient` instance.
-
-       Returns:
-           fabricv4.ApiClient: An instance of `fabricv4.ApiClient` that is authenticated
-           and ready to interact with the Equinix Fabric API.
-
-       Raises:
-           EnvironmentError: If the `EQUINIX_API_CLIENT_ID` or `EQUINIX_API_CLIENT_SECRET`
-           environment variables are not set.
-           OAuth2Error: If there is an error obtaining the access token from the OAuth2
-           service.
-
-       Example:
-           To use this function, ensure that the environment variables are set and call
-           it as follows:
-
-           ```python
-           client = get_equinix_fabric_client()
-           # Now you can use `client` to interact with the Equinix Fabric API
-           ```
-
-       Note:
-           This method is configured to work with the PROD (PRODUCTION)
-           environment of the Equinix API. If you need to connect to a different
-           environment, adjust the `access_token_url`, `authorize_url`, and `base_url`
-           accordingly.
-
        """
 
     client_id = os.getenv("EQUINIX_API_CLIENT_ID")
