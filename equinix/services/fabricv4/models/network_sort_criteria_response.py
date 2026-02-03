a62ac4faa6090d796e35b6b0348cc7412fdfca93
srushti-patl <srpatel@equinix.com>
2026-02-03 11:42:48 -0800

    fix: Updating SLASH_CHANGE_LOG_SLASH_UPDATED_DATE_TIME enum value

--- equinix/services/fabricv4/models/network_sort_criteria_response.py
+++ equinix/services/fabricv4/models/network_sort_criteria_response.py
@@ -25,7 +25,7 @@ class NetworkSortCriteriaResponse(BaseModel):
     NetworkSortCriteriaResponse
     """ # noqa: E501
     direction: Optional[NetworkSortDirectionResponse] = NetworkSortDirectionResponse.DESC
-    var_property: Optional[NetworkSortByResponse] = Field(default=NetworkSortByResponse.CHANGE_LOG_SLASH_UPDATED_DATE_TIME, alias="property")
+    var_property: Optional[NetworkSortByResponse] = Field(default=NetworkSortByResponse.SLASH_CHANGE_LOG_SLASH_UPDATED_DATE_TIME, alias="property")
     additional_properties: Dict[str, Any] = {}
     __properties: ClassVar[List[str]] = ["direction", "property"]
 
@@ -88,7 +88,7 @@ class NetworkSortCriteriaResponse(BaseModel):
 
         _obj = cls.model_validate({
             "direction": obj.get("direction") if obj.get("direction") is not None else NetworkSortDirectionResponse.DESC,
-            "property": obj.get("property") if obj.get("property") is not None else NetworkSortByResponse.CHANGE_LOG_SLASH_UPDATED_DATE_TIME
+            "property": obj.get("property") if obj.get("property") is not None else NetworkSortByResponse.SLASH_CHANGE_LOG_SLASH_UPDATED_DATE_TIME
         })
         # store additional fields in additional_properties
         for _key in obj.keys():
