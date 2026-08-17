ac3e4ca1944222c04ba91f8287f675a54bf77e7e
tomasz.tutka <tomasz.tutka@eu.equinix.com>
2026-08-17 14:39:45 +0200

    fix: correct IPBlocksApi class name in test helper

--- tests/services/fabricv4/helpers/apis.py
+++ tests/services/fabricv4/helpers/apis.py
@@ -24,7 +24,7 @@ class Apis:
         self.service_tokens = fabricv4.ServiceTokensApi(api_client)
         self.statistics = fabricv4.StatisticsApi(api_client)
         self.internet_access_services = fabricv4.InternetAccessServicesApi(api_client)
-        self.ip_blocks = fabricv4.IpBlocksApi(api_client)
+        self.ip_blocks = fabricv4.IPBlocksApi(api_client)
 
 
 def for_user(user_name: UserName) -> Apis:
